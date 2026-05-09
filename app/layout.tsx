66910355a80c9e6b0fa284ea4164846c7c328a95
ht751810808-dev <ht751810808@gmail.com>
2026-05-09 14:15:52 +0800

    feat: update GA4 measurement ID to G-8W5BXVHR03

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,14 +52,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          src="https://www.googletagmanager.com/gtag/js?id=G-8W5BXVHR03"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-XXXXXXXXXX');
+          gtag('config', 'G-8W5BXVHR03');
         `}</Script>
         {/* Google AdSense Auto Ads */}
         <Script
